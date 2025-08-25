6e6e7f2e536e9b856ee5e3c2aa135dc255a5d063
Arkya Patwa <arkyapatwa333@gmail.com>
2025-08-25 23:57:58 +0530

    configurations updated

--- src/conifg.py
+++ src/conifg.py
@@ -4,22 +4,21 @@ from pydantic_settings import BaseSettings
 
 load_dotenv()
 
-WATCHED_DIRECTORY = os.getenv("WATCHED_DIRECTORY", "./data")
+WATCHED_DIRECTORY = os.getenv("WATCHED_DIRECTORY", "./src/data")
 
 class OpenAIEmbeddingsConfig(BaseSettings):
     api_key: str = os.getenv("OPENAI_API_KEY", "")
-    model_name: str = os.getenv("OPENAI_MODEL_NAME", "gpt-3.5-turbo")
-    temperature: float = float(os.getenv("OPENAI_TEMPERATURE", "0.7"))
+    model_name: str = os.getenv("OPENAI_MODEL_NAME")
     max_retries: int = int(os.getenv("OPENAI_MAX_RETRIES", "3"))
     endpoint: str = os.getenv("OPENAI_ENDPOINT")
 
 class OpenAIChatConfig(BaseSettings):
-    api_key: str = os.getenv("OPENAI_API_KEY", "")
-    model_name: str = os.getenv("OPENAI_CHAT_MODEL_NAME", "gpt-3.5-turbo")
+    api_key: str = os.getenv("OPENAI_API_KEY")
+    model_name: str = os.getenv("OPENAI_CHAT_MODEL_NAME")
     temperature: float = float(os.getenv("OPENAI_CHAT_TEMPERATURE", "0.7"))
     max_retries: int = int(os.getenv("OPENAI_CHAT_MAX_RETRIES", "3"))
     
 class PineconeConfig(BaseSettings):
-    api_key: str = os.getenv("PINECONE_API_KEY", "")
-    environment: str = os.getenv("PINECONE_ENVIRONMENT", "")
-    index_name: str = os.getenv("PINECONE_INDEX_NAME", "pdf-index")
+    api_key: str = os.getenv("PINECONE_API_KEY")
+    index_name: str = os.getenv("PINECONE_INDEX_NAME")
+    namespace: str = os.getenv("PINECONE_NAMESPACE")
--- src/embeddings/openai_embeddings.py
+++ src/embeddings/openai_embeddings.py
@@ -1,12 +1,15 @@
-from embedding_generator_interface import EmbeddingGenerator
-from src.conifg import OpenAIEmbeddingsConfig
+from embeddings.embedding_generator_interface import EmbeddingGenerator
+from conifg import OpenAIEmbeddingsConfig
 from langchain_openai import OpenAIEmbeddings
 
 class OpenAIEmbeddingsGenerator(EmbeddingGenerator):
     def __init__(self, config: OpenAIEmbeddingsConfig):
         self.embeddings = OpenAIEmbeddings(
             model=config.model_name,
-            openai_api_key=config.api_key
+            api_key=config.api_key,
+            base_url=config.endpoint,
+            max_retries=config.max_retries,
+            dimensions=2000
         )
     
     def get_embedding_client(self) -> dict:
